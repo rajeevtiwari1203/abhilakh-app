d1851c7463a57fa2169bbf26b69f70c7dd4ef536
Ayush Kumar <ayush.wit@gmail.com>
2020-07-04 13:20:56 +0530

    contct us fixed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'contact',
@@ -7,8 +7,10 @@ import { FormBuilder, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  contactForm;
+  contactForm: FormGroup;
   @ViewChild('name') name: ElementRef;
+  @ViewChild('contact') contact: ElementRef;
+  @ViewChild('email') email: ElementRef;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -16,8 +18,8 @@ export class ContactComponent implements OnInit {
     let phoneNumberRegex = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$"; 
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, this.noWhiteSpaceValidator]],
-      contact: ['', [Validators.required, Validators.pattern(emailRegex), this.noWhiteSpaceValidator]],
-      email: ['', [Validators.required, Validators.pattern(phoneNumberRegex), this.noWhiteSpaceValidator]],
+      contact: ['', [Validators.required, Validators.pattern(phoneNumberRegex), this.noWhiteSpaceValidator]],
+      email: ['', [Validators.required, Validators.pattern(emailRegex), this.noWhiteSpaceValidator]],
       message: ['', [this.noWhiteSpaceValidator]],
     })
   }
@@ -25,6 +27,28 @@ export class ContactComponent implements OnInit {
     setTimeout(()=> {
       this.name.nativeElement.focus();
     }, 1000);
+  }
+  submit() {
+    if(this.contactForm.valid) {
+      alert('Your contact details are safe with us. We will contact you soon.');
+    } else {
+      if(this.contactForm.controls['name'].invalid) {
+        this.contactForm.controls['name'].markAsTouched();
+        setTimeout(() => {
+          this.name.nativeElement.focus();
+        }, 0);
+      } else if(this.contactForm.controls['contact'].invalid) {
+        this.contactForm.controls['contact'].markAsTouched();
+        setTimeout(() => {
+          this.contact.nativeElement.focus();
+        }, 0);
+      } else {
+        this.contactForm.controls['email'].markAsTouched();
+        setTimeout(() => {
+          this.email.nativeElement.focus();
+        }, 0);
+      }
+    }
   }
   private noWhiteSpaceValidator(control: FormControl) {
     const isWhitespace = (control.value || '').trim().length === 0;
