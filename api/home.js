1ad3f679f0089ff2f5920c6cd40a2b130566b42b
rajeevtiwari1203 <66266667+rajeevtiwari1203@users.noreply.github.com>
2020-09-16 22:39:24 +0530

    Update home.js

--- api/home.js
+++ api/home.js
@@ -19,49 +19,49 @@ router.get('/', async function (req, res, next) {
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
             },
             {
-                imgSrc: 'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg',
+                imgSrc: 'https://res.cloudinary.com/delffrxwf/image/upload/v1599994601/BANNER_rersa7.jpg',
                 text1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                 text2: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
                 text3: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below'
